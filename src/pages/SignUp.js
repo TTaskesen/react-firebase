cc89dfa3ce74b73dc4334b4fde24c0bd54d19ed4
turguttaskesen <taskesen@msn.com>
2023-05-25 17:25:54 +0300

    akşam

--- src/pages/SignUp.js
+++ src/pages/SignUp.js
@@ -1,6 +1,7 @@
 import { useState, useCallback } from "react"
-import { signInWithEmailAndPassword,createUserWithEmailAndPassword } from "firebase/auth"
+import { createUserWithEmailAndPassword } from "firebase/auth"
 import { auth } from "../firebase"
+import { Link } from "react-router-dom"
 
 const SignUp = () => {
 
@@ -12,16 +13,17 @@ const SignUp = () => {
         if (!email || !password) {
             return
         }
-        createUserWithEmailAndPassword(auth,email,password).then(()=>{alert("Başarılı Bir Şekilde giriş yaptınız.")}).catch((e)=>{console.log(e)})
+        createUserWithEmailAndPassword(auth,email,password).then(()=>{alert("Başarılı Bir Şekilde Kayıt oldunuz..")}).catch((e)=>{console.log(e)})
     }, [email,password])
 
     return (
         <div className="max-w-md mx-auto py-12">
-            <h1 className="text-2xl">Create New Account</h1>
+            <h1 className="text-2xl">Yeni Kullanıcı Oluştur</h1>
             <form onSubmit={handleSubmit} className="flex flex-col gap-4 mt-8">
                 <input type="email" placeholder="Mail Adresinizi Giriniz." className="p-4 bg-gray-100 rounded-md" value={email} onChange={(e) => setEmail(e.currentTarget.value)} />
                 <input type="password" placeholder="Password Giriniz." className="p-4 bg-gray-100 rounded-md" value={password} onChange={e => setPassword(e.currentTarget.value)} />
-                <input type="submit" className="p-4 bg-green-400 rounded-md" />
+                <input type="submit" className="p-4 bg-green-400 rounded-md" value="Kayıt Ol"/>
+                <Link to="/sign-in">Already have an account? Sign in</Link>
             </form>
         </div>
     )
